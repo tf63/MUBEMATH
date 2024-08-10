c572bf8667109d1c3533a6e2fcb07c8ea3e75bad
tf63 <74246282+tf63@users.noreply.github.com>
2024-08-11 00:15:47 +0900

    feat: improve stories in next app

--- apps/next/features/formula-input/formula-input.stories.tsx
+++ apps/next/features/formula-input/formula-input.stories.tsx
@@ -5,10 +5,10 @@ import type { Meta, StoryObj } from '@storybook/react'
 type FormulaInputType = typeof FormulaInput
 
 export default {
-    title: 'FormulaInput',
+    title: 'Next/FormulaInput',
     component: FormulaInput,
 } satisfies Meta<FormulaInputType>
 
 export const Default: StoryObj<FormulaInputType> = {
-    render: () => <FormulaInput />,
+    render: () => <FormulaInput inline={true} />,
 }
